25fbfe76c39ad68dea9d2422c977657c96a4ddac
KonstantinBenenson <km.benenson@gmail.com>
2025-01-19 21:43:05 +0300

    new requests

---
+++ src/GameBy.Common/GameBy.Common/Models/AddParticipantRequest.cs
@@ -0,0 +1,16 @@
+﻿using RatingService.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace RatingService.API.Models;
+
+public sealed class AddParticipantRequest
+{
+    [JsonPropertyName("participantId")]
+    public required int ExternalParticipantId { get; set; }
+    [JsonPropertyName("userId")]
+    public required int ExternalUserId { get; set; }
+    //[JsonPropertyName("eventId")]
+    //public int ExternalEventId { get; set; }
+    [JsonPropertyName("participation_state")]
+    public required ParticipationState State { get; set; }
+}
---
+++ src/GameBy.Common/GameBy.Common/Models/FinalizeEventRequest.cs
@@ -0,0 +1,23 @@
+﻿using RatingService.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace RatingService.API.Models.Events;
+
+public sealed class FinalizeEventRequest
+{
+    [JsonPropertyName("title")]
+    public required string Title { get; set; }
+    [JsonPropertyName("organizer_id")]
+    public required int OrganizerId { get; set; }
+    [JsonPropertyName("created_at")]
+    public required DateTime CreationDate { get; set; }
+    [JsonPropertyName("finished_at")]
+    public required DateTime FinishedDate { get; set; }
+    [JsonPropertyName("category")]
+    public required EventCategory Category { get; set; }
+    [JsonPropertyName("state")]
+    public required EventProgressionState State { get; set; }
+
+    [JsonPropertyName("participants")]
+    public required IEnumerable<AddParticipantRequest> Participants { get; set; }
+}
